dee763e719888da65acaa0841321c5fa29594ea4
Manuel Pedraza González <manuelpedrazag@gmail.com>
2022-06-01 19:58:32 -0400

    added a ROS2 service

--- ros2_ws/src/ros2_app_py/ros2_app_py/number_counter.py
+++ ros2_ws/src/ros2_app_py/ros2_app_py/number_counter.py
@@ -2,7 +2,7 @@
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Int64
-
+from my_robot_interfaces.srv import ResetCounter
 
 class NumberCounterNode(Node): 
   def __init__(self):
@@ -10,11 +10,22 @@ class NumberCounterNode(Node):
     self.counter_ = 0
     self.number_subscriber_ = self.create_subscription(
       Int64, "number", self.callback_number, 10)
+    self.reset_counter_service_ = self.create_service(
+      ResetCounter, "reset_counter", self.callback_reset_counter)
     self.get_logger().info("Number counter has been started.")
 
   def callback_number(self, msg):
     self.counter_ += msg.data
     self.get_logger().info("Counter: "+ str(self.counter_))
+  
+  def callback_reset_counter(self, request, response):
+    if(request.reset_value >= 0):
+      self.counter_ = request.reset_value
+      self.get_logger().info("Counter: "+ str(self.counter_))
+      response.success = True
+    else:
+      response.success = False
+    return response
 
 def main(args=None):
   rclpy.init(args=args)
